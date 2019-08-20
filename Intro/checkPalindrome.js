d182cc269e091ac3f23af76c266cf7a3b47c9379
Noah Yasskin <noahyasskin@gmail.com>
2019-08-19 20:07:04 -0700

    improve palindrome solution

--- Intro/checkPalindrome.js
+++ Intro/checkPalindrome.js
@@ -1,20 +1,43 @@
+// Given the string, check if it is a palindrome.
+
+// Example
+
+// For inputString = "aabaa", the output should be
+// checkPalindrome(inputString) = true;
+// For inputString = "abac", the output should be
+// checkPalindrome(inputString) = false;
+// For inputString = "a", the output should be
+// checkPalindrome(inputString) = true.
+// Input/Output
+
+// [execution time limit] 4 seconds (js)
+
+// [input] string inputString
+
+// A non-empty string consisting of lowercase characters.
+
+// Guaranteed constraints:
+// 1 ≤ inputString.length ≤ 105.
+
+// [output] boolean
+
+// true if inputString is a palindrome, false otherwise.
+
 function checkPalindrome(inputString) {
+        
+    // create a new array made up of the input's individual letters
+    let splitString = inputString.split("");
     
-    let characterArray = inputString.split("");
-    let reverseArray = [];
-    let possiblePalindrome = "";
+    // reverse the created array
+    let reverseArray = splitString.reverse();
     
-    characterArray.forEach(function(char){ 
-        reverseArray.unshift(char);
-    });
+    // joint all the array elements into a string
+    let possiblePalindrom = reverseArray.join("");
     
-    reverseArray.forEach(function(char){
-        possiblePalindrome = possiblePalindrome + char;       
-    });
-    
-    if (possiblePalindrome === inputString) {
+    if (possiblePalindrom === inputString) {
         return true;
     } else {
         return false;
     }
+    
 }
